0343c5f7a48e9e08786967b11e88ed14cd83c00e
Aliaksandr Kulba <tunet000@gmail.com>
2022-05-28 01:41:53 +0300

    feat: add factories

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -7,6 +7,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ */
 class Url extends Model
 {
     use HasFactory;
